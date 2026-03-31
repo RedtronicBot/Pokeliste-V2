6628fb2af68bac58d26481e70bcc950f96c48335
Redtronic <redtronicmc@gmail.com>
2026-03-31 20:15:27 +0200

    add provider

--- backend/src/sync/sync.module.ts
+++ backend/src/sync/sync.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common"
 import { SeriesModule } from "src/series/series.module"
 import { SetModule } from "src/set/set.module"
+import { SyncService } from "./sync.service"
 
 @Module({
   imports: [SeriesModule, SetModule],
+  providers: [SyncService],
 })
 export class SyncModule {}
