6edab3dccfbbfe12ed37e9c936cef78b06ccbf5d
Redtronic <redtronicmc@gmail.com>
2026-03-31 15:14:48 +0200

    non antoine

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
   })
   return (
     <div className="mx-auto flex w-full max-w-7xl flex-col items-center justify-center px-6 py-12">
-      <h1 className="mb-8 text-4xl font-black tracking-tight md:text-5xl">Extention Pokémon Trading Card Game</h1>
+      <h1 className="mb-8 text-4xl font-black tracking-tight md:text-5xl">Extension Pokémon Trading Card Game</h1>
       <div className="flex flex-col">
         {data
           ?.filter((series) => series.id !== "tcgp" && series.id !== "mc")
